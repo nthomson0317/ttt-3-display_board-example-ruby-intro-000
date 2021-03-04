689d1ef5d61f411da91e00d415e329ea7f97b89d
Nicholas Thomson <nicholasgraythomson@gmail.com>
2021-03-04 20:18:29 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,9 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
-def Tic_Tac_Toe_Board
+def display_board
   puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
 end
-Print Tic_Tac_Toe_Board
+display_board
